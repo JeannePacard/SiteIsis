d62d28f0be23817587dab6ea371785127a2d258e
JeannePacard <97599809+JeannePacard@users.noreply.github.com>
2022-04-25 08:55:21 +0200

    Update connection.js

--- js/connection.js
+++ js/connection.js
@@ -1,6 +1,5 @@
 const popup = document.querySelector(".user-popup");
 const button = document.querySelector(".user-button");
-const closebutton = document.querySelector(".close");
 
 button.addEventListener("click", function () {
   popup.style.display = "block";
